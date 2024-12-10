32dcadcdd552503731b941de62ab020385b39b44
Lanoire _ <plvlanoira07@gmail.com>
2024-12-11 03:26:56 +0800

    BINI MERCH 0.1.1
    added notes

--- MerchniLanoira-Home/Program.cs
+++ MerchniLanoira-Home/Program.cs
@@ -23,7 +23,15 @@ namespace MerchniLanoira_Home
  * Radomes, Kurt Jimuel A. (Reporter yan)
  */
 
-/* BETA VER 0.1.0 (Prototype)
+/* BETA VER 0.1.1 (Prototype)
  * First Prototype and Interfaces.
  * 100% Magical.
+ * ADDED NOTES SA MEMBER AT KAY JOHNREL PARENTE KUPAL
+ */
+
+/* notes lng po
+ * 1. Sa reporter yung exit app nasa homepage
+ * 2. Ayos ayos muna sa pag navigate hahaha
+ * 3. Shoutout kay JOHNREL PARENTE eto na bini merch mo (bwict k)
+ * 4. Habang report patugtugan nyo ng bini hahahaha
  */
